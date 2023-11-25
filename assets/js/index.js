e6a7d3dff21c950d377ed3f9a7ff554831e71fef
Joe Kassis <joerkassis@gmail.com>
2023-11-25 16:31:30 +0200

    Changed ALOT

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+
+document.getElementById("button-down").addEventListener("click", scrollThroughDown);
+function scrollThroughDown ()
+{
+    let width = screen.width;
+    if (width <= 980) {
+        var btn = document.getElementById("button-down");
+        btn.href = '#header';
+        console.log("width <= 980")
+    } else {
+        var btn = document.getElementById("button-down");
+        btn.href = '#nav';
+        console.log("width => 980")
+    }
+};
+
+document.getElementById("button-up").addEventListener("click", scrollThroughUp);
+function scrollThroughUp ()
+{
+    let width = screen.width;
+    if (width <= 980) {
+        var btn = document.getElementById("button-up");
+        btn.href = '#header';
+        console.log("width <= 980")
+    } else {
+        var btn = document.getElementById("button-up");
+        btn.href = '#nav';
+        console.log("width => 980")
+    }
+};
+
